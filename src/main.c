09d76766843da4855ab35d9ed620701f76d978fa
Pedro Garcia <pedrognunes139@gmail.com>
2023-03-31 19:44:31 -0300

    First Recursive Version

--- src/main.c
+++ src/main.c
@@ -4,77 +4,75 @@
 #include <unistd.h>
 #include "../include/utils.h"
 
-#define MAX_LINE_LENGTH  1000
 
 
-
-void printPoints(Point *pointsArray, int numberOfPoints){
-    for(int i = 0; i<numberOfPoints; i++){
-        printf("%d %d\n", pointsArray[i].x, pointsArray[i].y);
-    }
-}
-
-
-void inputRead(char *inputFileName, Point **pointsArray, Point *anchorA, Point *anchorB, int *numberOfPoints ){
-    char line[MAX_LINE_LENGTH];
-    int tempA, tempB, tempNumberOfpoints;
-
-    FILE *inputFile  = fopen(inputFileName, "r");
-    
-    for(int i = 0; ;i++){
-
-        if(!fgets(line, MAX_LINE_LENGTH, inputFile)){
-            break;
-        };
-
-        //primeira linha 
-        if(i == 0){
-            tempNumberOfpoints = atoi( strtok(line, " "));
-            *numberOfPoints = tempNumberOfpoints;
-
-            tempA = atoi( strtok(NULL, " "));
-            anchorA-> x = tempA;
-            anchorA-> y = 0;
-
-            tempB = atoi( strtok(NULL, " "));
-            anchorB-> x =  tempB;
-            anchorB-> y = 0;
-
-            *pointsArray = (Point *)malloc(tempNumberOfpoints * sizeof(Point));
-
-        }else{
-            (*pointsArray)[i-1].x = atoi( strtok(line, " "));
-            (*pointsArray)[i-1].y = atoi( strtok(NULL, " "));
-        }
-
-    }
-
-    fclose(inputFile);
-}
-
-
-/* Compara a cordenada y do ponto1 e do ponto2
-   retorna >0 se for maior =0 se for menor e <0 se for menor.
-*/
+/* 
+ *  Compara a cordenada y do ponto1 e do ponto2
+ *  retorna  n>0 se for maior n=0 se for menor e n<0 se for menor.
+ *  usada na operacao de qsort
+ */
 int cmpPointsY(const void *point1, const void *point2){
     int y1 = ((Point*)point1) -> y;
     int y2 = ((Point*)point2) -> y;
     return y1 - y2;
 }
 
-void bruteForce(Point *pointsArray){
 
-}
+/*
+ * Gera rucursivamente todas as sequencias possiveis de pontos e retorna
+ * a quantidade de pontos da maior sequencia gerada.
+ * 
+ * pointsArray : array dos pontos ordenados em relação a cordenada Y *precisa ser liberado usando free()
+ * numberOfPoints : quantidade de pontos
+ * index : index do ponto que a funcao recursiva sera chamada
+ * sequence : array auxiliar para guardar um sequencia de pontos
+ * seqIndex : index no array de sequencia 
+ * anchorA : ancora A
+ * anchorB : ancora B
+ */
+int generateSequences(Point *pointsArray, int numberOfPoints, int index, Point sequence[], int seqIndex, Point anchorA, Point anchorB, int maxSeq){
+
+    if (index >= numberOfPoints) {
+        // Imprime a sequência gerada
+        // printf("Sequence: ");
+        // for (int i = 0; i < seqIndex; i++) {
+        //     printf("(%d,%d) ", sequence[i].x, sequence[i].y);
+            
+        // }
+        // printf(" %d", seqIndex);
+        // printf("\n");
+
+        if(seqIndex > maxSeq){
+            maxSeq = seqIndex;
+        }
+
+        return maxSeq;
+    }
+
+
+    // Gera uma sequência que inclui o ponto atual
+    if (seqIndex == 0 || isAbove(anchorA, anchorB, sequence[seqIndex-1], pointsArray[index])) {
+        sequence[seqIndex] = pointsArray[index];
+        maxSeq = generateSequences(pointsArray, numberOfPoints, index+1, sequence, seqIndex+1, anchorA, anchorB, maxSeq);
+    }
+
+    
+    // Gera sequências que não incluem o ponto atual
+    maxSeq = generateSequences(pointsArray, numberOfPoints, index+1, sequence, seqIndex, anchorA, anchorB, maxSeq);
+};
+
+
 
 int main(int argc, char *argv[]){
+    int max = 0 ; //resultado
     int opt;
     char inputFileName[] = "test.txt";
     char *outputFileName = NULL;
 
-    int numberOfPoints; //need for qsort
+    int numberOfPoints;
     Point anchorA, anchorB; 
     Point *pointsArray = NULL;
-
+    // Point *sequence[numberOfPoints];
     // if ( argc < 5){
     //     fprintf(stderr,"Faltando argumentos! \n");
     //     return 0;
@@ -96,24 +94,17 @@ int main(int argc, char *argv[]){
     // }
 
 
-
+    // le o arquivo
     inputRead(inputFileName, &pointsArray, &anchorA, &anchorB, &numberOfPoints);
 
-
-
+    // ordena em ordem crescente os pontos em relacao a cordenada Y
     qsort(pointsArray, numberOfPoints, sizeof(Point), cmpPointsY);
 
-    printPoints(pointsArray, numberOfPoints);
+    Point sequence[numberOfPoints];
+    max = generateSequences(pointsArray, numberOfPoints, 0, sequence, 0, anchorA, anchorB, max);
+
+    printf("%d", max);
 
     free(pointsArray);
     return 0;
 }
-
-
-
-
-
-
-
-
-
--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,9 @@
 #include "../include/utils.h"
 
-
-/* Testa se um ponto esta acima das duas retas formadas pelas ancoras 
-e pelo ponto escolhido anteriormente*/
+/* 
+ * Testa se um ponto esta acima das duas retas formadas pelas ancoras 
+ * e pelo ponto escolhido anteriormente
+ */
 bool isAbove(Point anchorA, Point anchorB, Point prevP, Point testP){
 
 
@@ -10,7 +11,8 @@ bool isAbove(Point anchorA, Point anchorB, Point prevP, Point testP){
     float m2, b2;
 
 
-    /*if( anchorA.x == prevP.x ){   
+    if( anchorA.x == prevP.x ){  
+        //quando o ponto esta no mesmo eixo x q a ancora A
         m2 = (float)(prevP.y - anchorB.y) / (float)(prevP.x - anchorB.x);
         b2 = anchorB.y - (m2 * anchorB.x);
 
@@ -21,7 +23,7 @@ bool isAbove(Point anchorA, Point anchorB, Point prevP, Point testP){
         }
 
     }else if(anchorB.x == prevP.x){
-
+        //quando o ponto esta no mesmo eixo x q a ancora B
         m1 = (prevP.y - anchorA.y) / (prevP.x - anchorA.x);
         b1 = anchorA.y - (m1 * anchorA.x);
 
@@ -32,20 +34,60 @@ bool isAbove(Point anchorA, Point anchorB, Point prevP, Point testP){
         }
 
     }else{
-    */
+        //checa se o ponto de teste esta acima das duas retas ao mesmo tempo
+
+        //y = mx + b
+        m1 = (float)(prevP.y - anchorA.y) / (float)(prevP.x - anchorA.x);
+        b1 = (float)anchorA.y - (m1 * (float)anchorA.x);
+        m2 = (float)(prevP.y - anchorB.y) / (float)(prevP.x - anchorB.x);
+        b2 = (float)anchorB.y - (m2 * (float)anchorB.x);
 
 
-    m1 = (float)(prevP.y - anchorA.y) / (float)(prevP.x - anchorA.x);
-    b1 = (float)anchorA.y - (m1 * (float)anchorA.x);
-    m2 = (float)(prevP.y - anchorB.y) / (float)(prevP.x - anchorB.x);
-    b2 = (float)anchorB.y - (m2 * (float)anchorB.x);
-
-    //checa se o ponto de teste esta acima das duas retas ao mesmo tempo
-    if( testP.y >= (( m1 * testP.x ) + b1) && testP.y >= (( m2 * testP.x ) + b2) ){
-        return true;
-    }else{
-        return false;
+        if( testP.y >= (( m1 * testP.x ) + b1) && testP.y >= (( m2 * testP.x ) + b2) ){
+            return true;
+        }else{
+            return false;
+        }
     }
+}
 
 
+
+/*
+ * Lê o arquivo de entrada e serve as variadas recebidas por parametro com os valores lidos.
+ * Obs: pointsArray depois de usado prescisa ser liberado com Free().
+ */
+void inputRead(char *inputFileName, Point **pointsArray, Point *anchorA, Point *anchorB, int *numberOfPoints ){
+    char line[MAX_LINE_LENGTH];
+    int tempA, tempB, tempNumberOfpoints;
+
+    FILE *inputFile  = fopen(inputFileName, "r");
+    
+    for(int i = 0; ;i++){
+
+        if(!fgets(line, MAX_LINE_LENGTH, inputFile)){
+            break;
+        };
+
+        //primeira linha 
+        if(i == 0){
+            tempNumberOfpoints = atoi( strtok(line, " "));
+            *numberOfPoints = tempNumberOfpoints;
+
+            tempA = atoi( strtok(NULL, " "));
+            anchorA-> x = tempA;
+            anchorA-> y = 0;
+
+            tempB = atoi( strtok(NULL, " "));
+            anchorB-> x =  tempB;
+            anchorB-> y = 0;
+
+            *pointsArray = (Point *)malloc(tempNumberOfpoints * sizeof(Point));
+
+        }else{
+            (*pointsArray)[i-1].x = atoi( strtok(line, " "));
+            (*pointsArray)[i-1].y = atoi( strtok(NULL, " "));
+        }
+    }
+    fclose(inputFile);
 }
